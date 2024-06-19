ad367f70dbe9c2d020117aa04754a06212d952bd
Moee5055 <kshitij.moee55@gmail.com>
2024-06-19 10:13:50 +0545

    working on edit modal

--- src/app/_components/task/Display.tsx
+++ src/app/_components/task/Display.tsx
@@ -7,26 +7,50 @@ import {
   CardTitle,
 } from "@/components/ui/card";
 
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+
 const DisplayTask = () => {
   return (
     <div className="flex flex-col space-y-3 px-4">
       <div className="space-y-1 relative left-2">
-        <h1 className="text-xl font-bold text-muted-foreground">All Tasks</h1>
-        <div className="h-1 w-[70px] bg-muted-foreground rounded-lg"></div>
+        <h1 className="text-lg sm:text-xl font-bold text-muted-foreground">
+          All Tasks
+        </h1>
+        <div className="h-1 sm:[50px] sm:w-[70px] bg-muted-foreground rounded-lg"></div>
       </div>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 2xl:grid-cols-4 gap-4 pt-2">
-        <Card className="bg-background/70">
-          <CardHeader>
-            <CardTitle>Card Title</CardTitle>
-            <CardDescription>Card Description</CardDescription>
-          </CardHeader>
-          <CardContent>
-            <p>Card Content</p>
-          </CardContent>
-          <CardFooter>
-            <p>Card Footer</p>
-          </CardFooter>
-        </Card>
+        <Dialog>
+          <DialogTrigger>
+            <Card className="bg-background/70">
+              <CardHeader>
+                <CardTitle>Card Title</CardTitle>
+                <CardDescription>Card Description</CardDescription>
+              </CardHeader>
+              <CardContent>
+                <p>Card Content</p>
+              </CardContent>
+              <CardFooter>
+                <p>Card Footer</p>
+              </CardFooter>
+            </Card>
+          </DialogTrigger>
+          <DialogContent>
+            <DialogHeader>
+              <DialogTitle>Are you absolutely sure?</DialogTitle>
+              <DialogDescription>
+                This action cannot be undone. This will permanently delete your
+                account and remove your data from our servers.
+              </DialogDescription>
+            </DialogHeader>
+          </DialogContent>
+        </Dialog>
       </div>
     </div>
   );
